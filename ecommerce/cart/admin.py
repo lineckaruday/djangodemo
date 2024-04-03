4880879bfec0a6abaee6e5de6899ae87873230ec
lineckaruday <linakeruday@gmail.com>
2024-04-03 12:26:49 +0530

    added1

--- ecommerce/cart/admin.py
+++ ecommerce/cart/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from cart.models import Cart,Order,Account
+from django.http import HttpResponse           #added http just to know this updates in git (git repository project clone ,update)
 # Register your models here.
 
 admin.site.register(Cart)       #table name or model name Book
